820bf492c1494827bd3637b0320209960a0e575f
komalkumari12 <91238713+komalkumari12@users.noreply.github.com>
2022-01-26 23:10:17 +0530

    Time: 48 ms (25.66%), Space: 23.3 MB (11.96%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -4,15 +4,13 @@ public:
         if(root==NULL)
             return NULL ;
         
-        if((root->val > p->val && root->val < q->val) || (root->val < p->val && root->val > q->val))
-            return root ;
-        
         if(root->val > p->val && root->val > q->val)
             return lowestCommonAncestor(root->left, p , q) ;
         
         if(root->val < p->val && root->val < q->val)
             return lowestCommonAncestor(root->right, p , q) ;
         
+        else
         return root ;
     }
 };
