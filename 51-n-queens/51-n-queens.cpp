34d1b2418c2fb6193e062c5cedbf4916fff262de
komalkumari12 <91238713+komalkumari12@users.noreply.github.com>
2022-01-31 00:32:59 +0530

    Time: 8 ms (68.28%), Space: 7 MB (95.29%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,44 +1,59 @@
-// hashing
-
 class Solution {
 public:
-    void find(int col ,int n ,vector<string>&board ,vector<vector<string>>&ans, vector<int>&leftRow, vector<int>&upperDiag, vector<int>&lowerDiag){
-        if(col == n){
+    bool check(int row ,int col , vector<string>&board , int n){
+        int temp_row =row ;
+        int temp_col =col ;
+        
+        while(row>=0  &&  col>=0){
+            if(board[row][col]=='Q')
+                return false ;
+            row-- ;
+            col-- ;
+        }
+        
+        col=temp_col ;
+        row=temp_row ;
+        while(col>=0){
+            if(board[row][col]=='Q')
+                return false ;
+            col-- ;
+        }
+        
+        col=temp_col ;
+        row=temp_row ;
+        while(row<n  &&  col>=0 ){
+            if(board[row][col]=='Q')
+                return false ;
+            row++ ;
+            col-- ;
+        }
+        return true ;
+    }
+    
+    void solve(int col, int n, vector<string>&board ,vector<vector<string>>&ans ){
+        if(col==n){
             ans.push_back(board) ;
             return ;
         }
         
-        for(int row=0; row<n ; row++){
-            if(leftRow[row]==0  &&  upperDiag[n-1 + col-row]==0  &&  lowerDiag[col+row]==0){
-                board[row][col] = 'Q' ;
-                leftRow[row] ='Q' ;
-                upperDiag[n-1 + col-row] = 'Q' ;
-                lowerDiag[col + row] = 'Q' ;
-                
-                find(col+1 ,n , board, ans ,leftRow, upperDiag, lowerDiag) ;
-                
-                board[row][col] = '.' ;
-                leftRow[row] = 0 ;
-                upperDiag[n-1 + col-row] = 0 ;
-                lowerDiag[col + row] = 0 ;
+        for(int row=0 ;row< n ; row++){
+            if(check(row,col, board ,n)){
+                board[row][col]='Q' ;
+                solve(col+1,n ,board ,ans) ;
+                board[row][col]='.' ;
             }
-        }
+        }    
     }
-        
+    
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans ;
-        vector<string>board(n) ;
-        string s(n, '.') ;
+        vector<string> board(n) ;
+        string s(n ,'.') ;
         
-        for(int i=0 ; i<n ;i++)
-            board[i] = s;
+        for(int i= 0 ;i <n ;i ++)
+            board[i]=s ;
         
-        vector<int>leftRow(n, 0) ;
-        vector<int>upperDiag(2*n-1  ,0);
-        vector<int>lowerDiag(2*n -1, 0) ;
-        
-        find(0, n, board ,ans ,leftRow, upperDiag, lowerDiag) ;
-        
+        solve(0 ,n , board, ans) ;
         return ans ;
     }
 };
