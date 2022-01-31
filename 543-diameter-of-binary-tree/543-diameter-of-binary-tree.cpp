bb141a157d20083f93658ab82429d0923097d361
komalkumari12 <91238713+komalkumari12@users.noreply.github.com>
2022-02-01 00:27:00 +0530

    Time: 29 ms (8.04%), Space: 20.4 MB (25.68%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -1,20 +1,44 @@
+/* Approach
+    1, find lh and rh for every node
+    2. store max diagonal from lh and rh in maxDiag
+*/    
+
 class Solution {
-public:
-    int height(TreeNode*root, int&diameter){
+public: 
+    int leftHeight(TreeNode*root){
         if(root==NULL) return 0 ;
         
-        int lh = height(root->left, diameter) ;
-        int rh = height(root->right, diameter) ;
+        int lh=leftHeight(root->left) ;
+        int rh=leftHeight(root->right) ;
         
-        diameter=max(diameter, lh+rh) ;
+        return 1+ max(lh, rh) ;
+    }
+    
+    int rightHeight(TreeNode*root){
+        if(root==NULL) return 0 ;
         
-        return 1+ max(lh ,rh) ;
+        int lh=rightHeight(root->right) ;
+        int rh=rightHeight(root->left) ;
+        
+        return 1+ max(lh, rh) ;
+    }
+        
+    void heightHelper(TreeNode* root ,int&maxDia){
+        if(root==NULL) return  ;
+        
+        int lh=leftHeight(root->left) ;
+        int rh=rightHeight(root->right) ;
+        
+        maxDia=max(maxDia ,lh+rh) ;
+        
+        heightHelper(root->left,maxDia) ;
+        heightHelper(root->right,maxDia) ;
+        
     }
         
     int diameterOfBinaryTree(TreeNode* root) {
-        int diameter=0 ;
-        height(root,diameter) ;
-        
-        return diameter ;
+        int maxDia=0 ;
+        heightHelper(root ,maxDia) ;
+        return maxDia ;
     }
 };
