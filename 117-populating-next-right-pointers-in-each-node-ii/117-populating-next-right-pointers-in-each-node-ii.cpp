339b767015c189b23103c4e0ec57d459a2bf885d
komalkumari12 <91238713+komalkumari12@users.noreply.github.com>
2022-02-01 11:51:17 +0530

    Time: 15 ms (62.41%), Space: 17.5 MB (41.47%) - LeetHub

---
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(root==NULL) return NULL ;
+        Node*temp_root = root ;
+        queue<Node*> q ;
+        q.push(root) ;
+        
+        while(!q.empty()){
+            int size = q.size() ;
+            
+            for(int i= 0; i<size ; i++){
+                Node* node =q.front() ;
+                q.pop() ;
+                
+                if(i==size-1)
+                    node->next= NULL ;
+                else
+                    node->next= q.front() ;
+                
+                if(node->left)
+                    q.push(node->left) ;
+                if(node->right)
+                    q.push(node->right) ;
+            }
+        }
+        root=temp_root ;
+        return root ;
+    }
+};
