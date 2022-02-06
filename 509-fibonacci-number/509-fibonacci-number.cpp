d9ba9b7cd38ea9d5cdb009c392467b3f2184dc71
komalkumari12 <91238713+komalkumari12@users.noreply.github.com>
2022-02-07 01:01:23 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (39.43%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,18 +1,21 @@
 /* Approach
-    1.Use of concept of memoization
+    1.Use of concept of tabulation with constant space
 */    
 
 class Solution {
-public:
-    int fibo(vector<int>& dp ,int n){
-        if(n <= 1) return n ;
-        
-        if(dp[n] != -1) return dp[n] ;
-        return dp[n] = fibo(dp,n-1) + fibo(dp ,n-2) ;
-    }
-        
+public:    
     int fib(int n) {
-        vector<int> dp(n+1 ,-1) ;
-        return fibo(dp, n) ; 
+        int prev2 = 0 ;
+        int prev = 1 ;
+        
+        
+        if(n==0) return prev2 ;
+        
+        for(int i=2 ; i<=n ;i++){
+            int curri = prev + prev2 ;
+            prev2 = prev ;
+            prev = curri ;
+        }
+        return prev ;
     }
 };
