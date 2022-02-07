4adbaba4d7a7baea7195961ce3ca1254664602f4
komalkumari12 <91238713+komalkumari12@users.noreply.github.com>
2022-02-07 23:05:03 +0530

    Time: 4 ms (67.41%), Space: 6.7 MB (67.00%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,15 +1,22 @@
+/*  
+    Approach
+      a ^ a = 0
+      a ^ 0 = a
+      
+      ie. 0  0  0
+          0  1  0
+          1  0  1
+          1  1  0
+*/          
+
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        int sSum=0; 
-        int tSum=0;
+        int ans=0 ;
         
-        for(int i=0; i<s.size() ;i++)
-            sSum += s[i] ;
+        for(int i=0 ;i< t.size() ;i++) ans ^=t[i] ;
+        for(int i= 0; i<s.size() ;i++) ans^=s[i] ;
         
-        for(int i= 0; i<t.size() ;i++)
-            tSum += t[i] ;
-        
-        return tSum - sSum ;
+        return ans ;
     }
 };
