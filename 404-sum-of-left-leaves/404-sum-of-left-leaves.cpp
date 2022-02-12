5f0d15b9a19397b56c5f543a83bf3f53076d8b0e
komalkumari12 <91238713+komalkumari12@users.noreply.github.com>
2022-02-12 23:06:50 +0530

    Time: 6 ms (47.28%), Space: 13.6 MB (11.06%) - LeetHub

--- 404-sum-of-left-leaves/404-sum-of-left-leaves.cpp
+++ 404-sum-of-left-leaves/404-sum-of-left-leaves.cpp
@@ -1,28 +1,19 @@
 class Solution {
-public:   
+    int leftSum =0 ;
+public:
     bool isLeaf(TreeNode* root){
-        return (!root->left  &&   !root->right) ? true : false ;
+        return (!root->left  &&  !root->right) ? true : false ;
     }
         
     int sumOfLeftLeaves(TreeNode* root) {
-        if(!root) return 0;
+        if(!root) return 0 ;
         
-        queue<TreeNode*> q;
-        q.push(root) ;
-        int leftSum =0; 
+        if(root->left  &&  isLeaf(root->left))  leftSum += root->left->val ;
         
-        while(!q.empty()){
-            TreeNode* node = q.front() ;
-            q.pop() ;
-            
-            if(node->left){
-                q.push(node->left) ;
-                if(isLeaf(node->left))   leftSum += node->left->val ;
-            }
-            if(node->right){
-                q.push(node->right) ;
-            }
-        }
+        int left = sumOfLeftLeaves(root->left) ;
+        int right = sumOfLeftLeaves(root->right) ;
+        
+        
         return leftSum ;
     }
 };
