cc96ada8e8635b9ad8b88c0f2e7e868850747fc7
komalkumari12 <91238713+komalkumari12@users.noreply.github.com>
2022-02-13 01:05:38 +0530

    Time: 22 ms (35.00%), Space: 20.8 MB (94.73%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -1,41 +1,22 @@
-/* Approach
-     1.Find lh and rh of every node 
-     2.if absolute difference of lh and rh is greater than 1 return false
-*/
-
 class Solution {
 public:
-    int leftHeight(TreeNode*root){
-        if(root==NULL) return 0 ;
+    int height(TreeNode* root){
+        if(!root) return 0 ;
         
-        int lh=leftHeight(root->left) ;
-        int rh=leftHeight(root->right) ;
+        int lh=height(root->left) ;
+        if(lh==-1) return -1 ;
         
-        return 1+ max(lh,rh) ;
-    }
-    
-    int rightHeight(TreeNode*root){
-        if(root==NULL) return 0 ;
+        int rh=height(root->right) ;
+        if(rh==-1) return -1 ;
         
-        int rh=rightHeight(root->right) ;
-        int lh=rightHeight(root->left) ;
+        if(abs(lh-rh)>1) return -1 ;
         
         return 1+ max(lh,rh) ;
     }
-     
+        
     bool isBalanced(TreeNode* root) {
-        if(root==NULL) return true ;
-        
-        int lh= leftHeight(root->left) ;
-        int rh= rightHeight(root->right) ;
-        
-        if(abs(lh-rh)>1) return false ;
-        
-        bool left=isBalanced(root->left) ;
-        bool right=isBalanced(root->right) ;
-        
-        if(!left || !right) return false ;
-        else return true ;
-    
+        int result = height(root) ;
+        if(result == -1) return false ;
+        else  return true ;
     }
 };
