84683961ea7bebcfe6e2781990f3181afb4fa9f3
komalkumari12 <91238713+komalkumari12@users.noreply.github.com>
2022-02-12 20:55:51 +0530

    Time: 43 ms (72.36%), Space: 39 MB (25.42%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -1,38 +1,14 @@
-/* Approach
-    1. store values of node in a vector in ascending order through inorder
-    2. find 2 sum in a vector
-*/    
-
 class Solution {
+private: 
+    unordered_set<int> set ;
 public:
-    vector<int> ans ;
-    
-    void inorder(TreeNode* root){
-        if(root==NULL) return ;
+    bool findTarget(TreeNode* root, int target ) {
+        if(!root) return false ;
         
-        inorder(root->left) ;
-        ans.push_back(root->val) ;
-        inorder(root->right) ;
-    }
-    
-    bool findTarget(TreeNode* root, int key) {
-        inorder(root) ;      
+        if(set.find(target - root->val) != set.end()) return true ;
+        set.insert(root->val) ;
         
-        int i=0, j=ans.size() -1 ;
-        
-        while(i<j){
-            if(ans[i] + ans[j]== key)
-                return true ;
-            else if(ans[i] + ans[j] > key)
-                j-- ;
-            else if(ans[i] + ans[j] < key)
-                i++ ;
-        }
-        return false ;
+        return findTarget(root->left ,target) ||  findTarget(root->right ,target) ;
     }
 };
-
-/*
-note: j should be strictly greater than than i while traversing the vector
-          tree-[1] ,key-2 ......in this case (ans[i] + ans[j]==2) which gives true 
-*/          
+        
