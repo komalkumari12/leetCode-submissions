3b1d9c2533239f063cd11d2895ce36e866a44ed3
komalkumari12 <91238713+komalkumari12@users.noreply.github.com>
2022-02-18 22:30:41 +0530

    Time: 8 ms (98.16%), Space: 8.9 MB (41.86%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -2,16 +2,20 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         vector<int> mag(26,0) ;
+        vector<int> ran(26,0) ;
         
         for(int i=0; magazine[i] != '\0' ;i++){
             mag[magazine[i] - 'a']++ ;
         }
         
         for(int i=0; ransomNote[i] != '\0' ;i++){
-           if( mag[ransomNote[i] - 'a'] ==0) return false ;
-            mag[ransomNote[i] - 'a']-- ;
+            ran[ransomNote[i] - 'a']++ ;
         }
         
+        
+        for(int i=0; i<26 ;i++){
+            if(mag[i] < ran[i]) return false ;
+        }
         return true ;
         
     }
