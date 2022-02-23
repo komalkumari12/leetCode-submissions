d394b174f565c0982a58d100dd9170d62fe703c5
komalkumari12 <91238713+komalkumari12@users.noreply.github.com>
2022-02-23 13:55:10 +0530

    Time: 67 ms (52.42%), Space: 41.4 MB (81.38%) - LeetHub

---
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    int sum =0 ;
+public:
+    int sumEvenGrandparent(TreeNode* root) {
+        if(!root) return 0 ;
+        
+        if(root->val%2 == 0){
+            if(root->left){
+                if(root->left->left)  sum+=root->left->left->val ;
+                if(root->left->right) sum+=root->left->right->val ;
+            }
+            if(root->right){
+                if(root->right->left)  sum+=root->right->left->val ;
+                if(root->right->right) sum+=root->right->right->val ;
+            }
+        }
+        
+        sumEvenGrandparent(root->left);
+        sumEvenGrandparent(root->right) ;
+        
+        return sum ;
+        
+    }
+};
