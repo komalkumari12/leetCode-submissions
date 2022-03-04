f98afcf60f24cd0ef58c3e132cf80d1c6fc59b11
komalkumari12 <91238713+komalkumari12@users.noreply.github.com>
2022-03-04 10:12:03 +0530

    Time: 40 ms (66.53%), Space: 10.7 MB (70.43%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -3,13 +3,13 @@ public:
     int firstUniqChar(string s) {
         vector<int> freq(26,0) ;
         
-        for(int i =0; s[i] != '\0' ;i++){
-            freq[s[i] - 'a']++ ;
-        }
+        for(int i=0; s[i]!='\0' ;i++)
+            freq[s[i]-'a']++ ;
         
-        for(int i=0; s[i] != '\0' ;i++){
-            if(freq[s[i] - 'a']==1) return i ;
-        }
+        for(int i=0; s[i]!='\0' ;i++)
+            if(freq[s[i]-'a'] == 1)
+                return i ;
+        
         return -1 ;
     }
 };
