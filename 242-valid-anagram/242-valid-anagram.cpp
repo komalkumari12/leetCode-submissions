4fb835a4c5737a8da27db7600b4259921feed947
komalkumari12 <91238713+komalkumari12@users.noreply.github.com>
2022-03-04 10:18:10 +0530

    Time: 3 ms (98.99%), Space: 7.4 MB (47.12%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -3,14 +3,14 @@ public:
     bool isAnagram(string s, string t) {
         if(s.size() != t.size()) return false ;
         
-        vector<int> sHash(256 ,0) ; 
-        vector<int> tHash(256 ,0) ; 
+        vector<int> sfreq(26,0) ;
+        vector<int> tfreq(26,0) ;
         
         for(int i=0; s[i] != '\0' ;i++){
-            sHash[s[i] - 'a']++ ;
-            tHash[t[i] - 'a']++ ;
+            sfreq[s[i]-'a']++ ;
+            tfreq[t[i]-'a']++ ;
         }
         
-        return sHash == tHash ? true : false ; 
+        return sfreq==tfreq ? true : false ;
     }
 };
