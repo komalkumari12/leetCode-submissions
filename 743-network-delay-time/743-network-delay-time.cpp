5fd3680d268376f7ffed61c5e0022e83566c39ab
komalkumari12 <91238713+komalkumari12@users.noreply.github.com>
2022-05-15 10:15:16 +0530

    Time: 145 ms (66.74%), Space: 39.9 MB (71.79%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,38 +1,44 @@
 class Solution {
 public:
-     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-            vector<vector<pair<int,int>>> adjlist(n+1);
-     
-        for(auto ele:times){
-            adjlist[ele[0]].push_back({ele[1],ele[2]});    
+    int networkDelayTime(vector<vector<int>>& times, int n, int src) {
+        vector<vector<pair<int,int>>> graph(n+1) ;
+        
+        for(int i=0; i<times.size(); i++){
+            graph[times[i][0]].push_back({times[i][1], times[i][2]}) ;
         }
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        vector<int> dist(n+1,INT_MAX);
-        vector<bool> visited(n+1,false);
-        dist[k]=0;
-        pq.push({0,k}); 
+        
+        // for(auto it: times){
+        //     graph[it[0]].push_back({it[1], it[2]}) ;
+        // }
+        
+        vector<int> dist(n+1, 1e9) ;
+        dist[src] =0 ;
+        
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq ;
+        pq.push({0,src}) ;       // distance , node
         
         while(!pq.empty()){
-            int node=pq.top().second;
-            int cost=pq.top().first;
-             visited[node]=true;
-            pq.pop();
-            for(auto ele : adjlist[node]){
-                int next_node=ele.first;
-                int cost=ele.second;
-                if(!visited[next_node] && dist[next_node]>dist[node]+cost){
-                    dist[next_node]=dist[node]+cost;
-                    pq.push({dist[next_node],next_node});
+            int node = pq.top().second ;
+            int srcDist = pq.top().first ;
+            pq.pop() ;
+            
+            for(auto it: graph[node]){
+                int adjNode = it.first ;
+                int weight = it.second ;
+                
+                if(srcDist + weight < dist[adjNode]){
+                    dist[adjNode] = srcDist + weight  ;
+                    pq.push({dist[adjNode] ,adjNode}) ;
                 }
             }
         }
         
-        int ans=INT_MIN;
-        for(int i=1;i<dist.size();i++) ans=max(ans,dist[i]);
+        int maxTime =0 ;
+        for(int i=1; i<dist.size(); i++){
+            if(dist[i] == 1e9) return -1 ;
+            maxTime = max(maxTime, dist[i]) ;
+        }
         
-        if(ans==INT_MAX) return -1;
-        return ans;
-        
-        
+        return maxTime ;
     }
 };
