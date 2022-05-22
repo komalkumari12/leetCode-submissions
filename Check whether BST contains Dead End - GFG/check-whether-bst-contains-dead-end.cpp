36709618b3911f4fc849425ab0bf7fb7c7536a79
komalkumari12 <91238713+komalkumari12@users.noreply.github.com>
2022-05-22 21:25:18 +0530

    Added solution - LeetHub

---
+++ Check whether BST contains Dead End - GFG/check-whether-bst-contains-dead-end.cpp
@@ -0,0 +1,119 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    data = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+
+void insert(Node ** tree, int val)
+{
+    Node *temp = NULL;
+    if(!(*tree))
+    {
+        temp = new Node(val);
+        *tree = temp;
+        return;
+    }
+
+    if(val < (*tree)->data)
+    {
+        insert(&(*tree)->left, val);
+    }
+    else if(val > (*tree)->data)
+    {
+        insert(&(*tree)->right, val);
+    }
+
+}
+
+
+
+
+int getCountOfNode(Node *root, int l, int h)
+{
+
+    if (!root) return 0;
+
+
+    if (root->data == h && root->data == l)
+        return 1;
+
+    if (root->data <= h && root->data >= l)
+         return 1 + getCountOfNode(root->left, l, h) +
+                    getCountOfNode(root->right, l, h);
+
+    else if (root->data < l)
+         return getCountOfNode(root->right, l, h);
+
+
+    else return getCountOfNode(root->left, l, h);
+}
+
+
+bool isDeadEnd(Node *root);
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        Node *root;
+        Node *tmp;
+    //int i;
+
+        root = NULL;
+
+        int N;
+        cin>>N;
+        for(int i=0;i<N;i++)
+        {
+            int k;
+            cin>>k;
+            insert(&root, k);
+
+        }
+
+        
+     cout<<isDeadEnd(root);
+     cout<<endl;
+    }
+}
+
+// } Driver Code Ends
+
+
+/*The Node structure is
+struct Node {
+int data;
+Node * right, * left;
+};*/
+
+/*You are required to complete below method */
+
+bool find(Node* root, int minVal, int maxVal){
+    if(!root) return false ;
+    
+    if(!root->left && !root->right){
+        if(minVal == maxVal)  return true ;             // dead end present
+        return false ;
+    } 
+    
+    return find(root->left, minVal, root->data -1) || find(root->right, root->data+1, maxVal) ;
+}
+
+bool isDeadEnd(Node *root)
+{
+    return find(root, 1, INT_MAX) ;
+}
