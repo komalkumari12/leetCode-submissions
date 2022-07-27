a7263dd27a1eddc4f5815495ec59fd6aedfa8959
komalkumari12 <91238713+komalkumari12@users.noreply.github.com>
2022-07-27 20:55:09 +0530

    Time: 61 ms (17.91%), Space: 24.2 MB (54.54%) - LeetHub

--- 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
@@ -1,30 +1,31 @@
-//TC: O(1)
-//SC: O(n)
-
 class BSTIterator {
 public:
-    vector<int> in ;
-    int top =0 ;
+    stack<TreeNode*> st ;
     
+    void inorder(TreeNode* root){
+        
+        while(root){
+            st.push(root) ;
+            root= root->left ;
+        }
+    }
+        
     BSTIterator(TreeNode* root) {
         inorder(root) ;
     }
     
-    void inorder(TreeNode* root){
-        if(!root) return  ;
-        
-        inorder(root->left) ;
-        in.push_back(root->val) ;
-        inorder(root->right) ;
-    }
-        
     int next() {
-        return in[top++] ;
+        TreeNode* node = st.top() ;
+        st.pop() ;
+        
+        if(node->right)
+            inorder(node->right) ;
+        
+        return node->val ;
     }
     
     bool hasNext() {
-        if(top < in.size())
-            return true;
-        return false ;
+        if(st.empty()) return false;
+        return true ;
     }
 };
