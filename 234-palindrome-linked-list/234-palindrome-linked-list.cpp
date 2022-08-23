36c0aa179244c9812a36652c5753d5c6b94d79b3
komalkumari12 <91238713+komalkumari12@users.noreply.github.com>
2022-08-23 22:00:53 +0530

    Time: 301 ms (72.62%), Space: 114.1 MB (74.71%) - LeetHub

---
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head){
+        ListNode* curr= head, *ptr= NULL ;
+        
+        while(head){
+            curr = head ;
+            head=head->next ;
+            curr->next = ptr ;
+            ptr = curr ;
+        }
+        
+        return ptr ;
+    }
+        
+    bool isPalindrome(ListNode* head) {
+        // find middle
+        // reverse second half
+        // check for palindrome
+        
+        if(head==NULL || head->next==NULL)
+            return true ;
+        
+        ListNode* fast = head, *slow = head ;
+        while(fast && fast->next){
+            fast = fast->next->next ;
+            slow = slow->next ;
+        }
+        
+        slow = reverseList(slow) ;
+        
+        while(slow != NULL){
+            if(slow->val != head->val)  return false ;
+            slow= slow->next ;
+            head= head->next ;
+        }
+        
+        return true ;
+    }
+};
