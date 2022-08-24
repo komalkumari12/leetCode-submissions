964896c0fcf3b5891d2e4e95b93f00496e6bef7b
komalkumari12 <91238713+komalkumari12@users.noreply.github.com>
2022-08-24 23:56:40 +0530

    Time: 80 ms (6.57%), Space: 25.1 MB (5.55%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.cpp
+++ 1446-consecutive-characters/1446-consecutive-characters.cpp
@@ -1,47 +1,48 @@
-// class Solution {
-// public:
-//     int maxPower(string s) {
-//         int maxP = INT_MIN ;
-//         int left =0, right =0 ;
-        
-//         for(int i=0; i<s.size(); i++){
-//             set<char> st ;
-            
-//             for(int i=left; i<= right; i++){
-//                 st.insert(s[i]) ;
-            
-//             }
-            
-//             if(st.size() == 1){
-//                 maxP = max(maxP, right- left+1) ;
-//             }
-//             else{
-//                 left++ ;
-//             }
-          
-//             right++ ;
-//         }
-        
-//         return maxP ;
-//     }
-// };
-
 class Solution {
 public:
     int maxPower(string s) {
-        int maxP = 1 ;
-        int count =1 ;
+        int maxP = INT_MIN ;
+        int left =0, right =0 ;
+        
+        for(int i=0; i<s.size(); i++){
+            set<char> st ;
             
-        for(int i=0; i<s.size()-1; i++){
-            if(s[i] == s[i+1]){
-                count++ ;
-                maxP = max(maxP, count) ;
-            }  
-            else{
-                count =1 ;
+            for(int i=left; i<= right; i++){
+                st.insert(s[i]) ;
+            
             }
+            
+            if(st.size() == 1){
+                maxP = max(maxP, right- left+1) ;
+            }
+            else{
+                left++ ;
+            }
+            
+            right++ ;
+            if(right == s.size())   break ;
         }
         
         return maxP ;
     }
 };
+
+// class Solution {
+// public:
+//     int maxPower(string s) {
+//         int maxP = 1 ;
+//         int count =1 ;
+            
+//         for(int i=0; i<s.size()-1; i++){
+//             if(s[i] == s[i+1]){
+//                 count++ ;
+//                 maxP = max(maxP, count) ;
+//             }  
+//             else{
+//                 count =1 ;
+//             }
+//         }
+        
+//         return maxP ;
+//     }
+// };
