57128f1bf751b3ca3558278ca705965c01723af6
komalkumari12 <91238713+komalkumari12@users.noreply.github.com>
2022-10-12 00:29:10 +0530

    Time: 1 ms (69.14%), Space: 6.2 MB (8.44%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -4,13 +4,16 @@ public:
         if(num == 0)    return 0 ;
         
         vector<int>dp(num+1) ;
-        dp[0] =0 ;
-        dp[1] =1 ;
+        int prev2 =0 ;
+        int prev1 =1 ;
         
         for(int i=2; i<=num; i++){
-            dp[i] = dp[i-1] + dp[i-2] ;
+            int curr = prev1 + prev2 ;
+            
+            prev2 = prev1 ;
+            prev1 = curr ;
         }
         
-        return dp[num] ;
+        return prev1 ;
     }
 };
