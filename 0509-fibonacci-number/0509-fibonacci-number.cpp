0cb91bb57c5a320a5d953df36a632633c132412d
komalkumari12 <91238713+komalkumari12@users.noreply.github.com>
2022-10-12 00:18:20 +0530

    Time: 5 ms (53.21%), Space: 6.2 MB (8.44%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,20 +1,16 @@
-//Using map
-
 class Solution {
 public:
-    int fibonacciNum(int num, map<int,int>& dp){
-        if(num <= 1)
-            return num ;
-        
-        if(dp.find(num) != dp.end())
-            return dp[num] ;
-        
-        dp[num] = fibonacciNum(num-1, dp) + fibonacciNum(num-2, dp) ;
-        return dp[num] ;
-    }
     int fib(int num) {
+        if(num == 0)    return 0 ;
         
-        map<int,int> dp ;
-        return fibonacciNum(num, dp) ;
+        vector<int>dp(num+1) ;
+        dp[0] =0 ;
+        dp[1] =1 ;
+        
+        for(int i=2; i<=num; i++){
+            dp[i] = dp[i-1] + dp[i-2] ;
+        }
+        
+        return dp[num] ;
     }
 };
