8ce8f9015d11e8ae325b91e5a974b8dd0b6730d0
komalkumari12 <91238713+komalkumari12@users.noreply.github.com>
2022-11-18 22:56:15 +0530

    Added solution - LeetHub

---
+++ Preorder Traversal and BST - GFG/preorder-traversal-and-bst.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    
+    
+    int canRepresentBST(int arr[], int N) {
+        stack<int> st ;
+        int root = INT_MIN ;
+        
+        for(int i=0; i<N; i++){
+            if(arr[i] <= root)   return 0 ;
+            
+            while(!st.empty()  &&  st.top() < arr[i]){
+                root = st.top() ;
+                st.pop() ;
+            }
+            
+            st.push(arr[i]) ;
+        }
+        
+        return 1 ;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        int arr[N];
+        for (int i = 0; i < N; i++) cin >> arr[i];
+        Solution ob;
+        cout << ob.canRepresentBST(arr, N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
