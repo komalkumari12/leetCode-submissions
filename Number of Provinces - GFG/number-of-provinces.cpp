485804e0456f3ab6a28c90d0051c368de1e4029d
komalkumari12 <91238713+komalkumari12@users.noreply.github.com>
2022-11-19 12:45:51 +0530

    Added solution - LeetHub

---
+++ Number of Provinces - GFG/number-of-provinces.cpp
@@ -0,0 +1,112 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class DisjoinSet{
+    public:
+        vector<int> parent, rank, size ;
+    
+        DisjoinSet(int n){
+            parent.resize(n+1) ;
+            rank.resize(n+1, 0) ;
+            size.resize(n+1, 1) ;
+            
+            for(int i=0; i<n+1; i++)
+                parent[i] = i ;
+        }
+        
+        int findParent(int node){
+            if(parent[node] == node)
+                return node ;
+            return parent[node] = findParent(parent[node]) ;
+        }
+        
+        void unionByRank(int u, int v){
+            int par_u = findParent(u) ;
+            int par_v = findParent(v) ;
+            
+            if(par_u == par_v)  return ;
+            if(rank[par_v] < rank[par_u]){
+                parent[par_v] = par_u ;
+            }
+            else if(rank[par_v] > rank[par_u]){
+                parent[par_u] = par_v ;
+            }
+            else{
+                parent[par_v] = par_u ;
+                rank[par_u]++ ;
+            }
+        }
+        
+        void unionBySize(int u, int v){
+            int par_u = findParent(u) ;
+            int par_v = findParent(v) ;
+            
+            if(par_u == par_v)  return ;
+            if(size[par_u] < size[par_v]){
+                parent[par_u] = par_v ;
+                size[par_v] += size[par_u] ;
+            }
+            else{
+                parent[par_v] = par_u ;
+                size[par_u] += size[par_v] ;
+            }
+        }
+};
+
+class Solution {
+  public:
+    int numProvinces(vector<vector<int>> adj, int V) {
+        DisjoinSet ds(V) ;
+        
+        for(int i=0; i<V; i++){
+            for(int j=0; j<V; j++){
+                if(adj[i][j] == 1){
+                    ds.unionBySize(i, j) ;
+                }
+            }
+        }
+        
+        int count =0 ;
+        for(int i=0; i<V; i++){
+            if(ds.parent[i] == i)
+                count++ ;
+        }
+        
+        return count ;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int V,x;
+        cin>>V;
+        
+        vector<vector<int>> adj;
+        
+        for(int i=0; i<V; i++)
+        {
+            vector<int> temp;
+            for(int j=0; j<V; j++)
+            {
+                cin>>x;
+                temp.push_back(x);
+            }
+            adj.push_back(temp);
+        }
+        
+
+        Solution ob;
+        cout << ob.numProvinces(adj,V) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
