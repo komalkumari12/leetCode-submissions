418bf07876d724a75d4068346008777bcaba4cd8
komalkumari12 <91238713+komalkumari12@users.noreply.github.com>
2022-12-05 01:37:35 +0530

    Time: 298 ms (85.81%), Space: 58.9 MB (63.01%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
@@ -1,53 +1,32 @@
 class Solution {
-    int dr[4] = {0, -1, 0, 1} ;
-    int dc[4] = {-1, 0, 1, 0} ;
-    
     bool inRange(int i, int j, int n, int m){
         if(i>=0 && j>=0 && i<n && j<m)
             return true ;
         return false ; 
     }
     
-    bool checkCycle(int i, int j, vector<vector<int>>& vis, vector<vector<char>>& grid){
-        //BFS
-        int n = grid.size() ;
-        int m = grid[0].size() ;
+    bool checkCycle(int i, int j, int n, int m, char tar, vector<vector<int>>& vis, vector<vector<char>>& grid){
+        if(!inRange(i, j, n, m) || grid[i][j] != tar)
+            return false ;
         
-        queue<pair<pair<int,int>, pair<int,int>>> q;
-        q.push({{i, j}, {-1, -1}}) ;
-        vis[i][j]=1 ;
+        if(vis[i][j] == 1)   return true ;
         
-        while(!q.empty()){
-            int node_row = q.front().first.first ;
-            int node_col = q.front().first.second ;
-            int par_row = q.front().second.first ;
-            int par_col = q.front().second.second ;
-            q.pop() ;
+        vis[i][j] = 1 ;
+        char ch = grid[i][j] ;
+        grid[i][j] = '1' ;
             
-            for(int ind=0; ind<4; ind++){
-                int new_row = node_row + dr[ind] ;
-                int new_col = node_col + dc[ind] ;
-                
-                if(!inRange(new_row, new_col, n, m) || grid[new_row][new_col] != grid[node_row][node_col])
-                    continue ;
-                
-                if(vis[new_row][new_col]!=1){
-                    //not visited
-                    vis[new_row][new_col]=1 ;
-                    q.push({{new_row, new_col}, {node_row, node_col}}) ;
-                }
-                else{
-                    if((new_row != par_row) && (new_col != par_col))
-                        return true ;
-                }
-            }
-        }
+        bool ans = checkCycle(i+1, j, n, m, tar, vis, grid) || 
+            checkCycle(i-1, j, n, m, tar, vis, grid) || 
+            checkCycle(i, j+1, n, m, tar, vis, grid) || 
+            checkCycle(i, j-1, n, m, tar, vis, grid) ;
         
-        return false ;
+        grid[i][j] = ch ;    
+        return ans ;
     }
-        
+         
 public:
     bool containsCycle(vector<vector<char>>& grid) {
+        //DFS
         int n = grid.size() ;
         int m = grid[0].size() ;
         vector<vector<int>> vis(n, vector<int>(m, 0)) ;
@@ -55,7 +34,7 @@ public:
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(!vis[i][j]){
-                    if(checkCycle(i, j, vis, grid))
+                    if(checkCycle(i, j, n, m, grid[i][j], vis, grid))
                         return true ;
                 }
             }
