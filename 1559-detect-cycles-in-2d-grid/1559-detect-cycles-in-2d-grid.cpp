ac54a9495792e864b0a415ef83bba8365eb0e23f
komalkumari12 <91238713+komalkumari12@users.noreply.github.com>
2022-12-05 02:23:42 +0530

    Time: 620 ms (54.19%), Space: 63.4 MB (46.24%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
@@ -1,41 +1,89 @@
+class DisjoinSet{
+    vector<int> parent, rank, size ;
+    public:
+        DisjoinSet(int n){
+            parent.resize(n+1) ;
+            rank.resize(n+1, 0) ;
+            size.resize(n+1, 1) ;
+            
+            for(int i=0; i<n+1; i++)
+                parent[i] = i ;
+        }
+        
+        int findParent(int node){
+            if(parent[node] == node)
+                return node ;
+            return parent[node] = findParent(parent[node]) ;
+        }
+        
+        void unionByRank(int u, int v){
+            int par_u = findParent(u) ;
+            int par_v = findParent(v) ;
+            
+            if(par_u == par_v)  return ;
+            if(rank[par_v] < rank[par_u]){
+                parent[par_v] = par_u ;
+            }
+            else if(rank[par_v] > rank[par_u]){
+                parent[par_u] = par_v ;
+            }
+            else{
+                parent[par_v] = par_u ;
+                rank[par_u]++ ;
+            }
+        }
+        
+        void unionBySize(int u, int v){
+            int par_u = findParent(u) ;
+            int par_v = findParent(v) ;
+            
+            if(par_u == par_v)  return ;
+            if(size[par_u] < size[par_v]){
+                parent[par_u] = par_v ;
+                size[par_v] += size[par_u] ;
+            }
+            else{
+                parent[par_v] = par_u ;
+                size[par_u] += size[par_v] ;
+            }
+        }
+};
+
 class Solution {
+    int dr[4] = {0, -1, 0, 1} ;
+    int dc[4] = {-1, 0, 1, 0} ;
+    
     bool inRange(int i, int j, int n, int m){
         if(i>=0 && j>=0 && i<n && j<m)
             return true ;
         return false ; 
     }
     
-    bool checkCycle(int i, int j, int n, int m, char tar, vector<vector<int>>& vis, vector<vector<char>>& grid){
-        if(!inRange(i, j, n, m) || grid[i][j] != tar)
-            return false ;
-        
-        if(vis[i][j] == 1)   return true ;
-        
-        vis[i][j] = 1 ;
-        char ch = grid[i][j] ;
-        grid[i][j] = '1' ;
-            
-        bool ans = checkCycle(i+1, j, n, m, tar, vis, grid) || 
-            checkCycle(i-1, j, n, m, tar, vis, grid) || 
-            checkCycle(i, j+1, n, m, tar, vis, grid) || 
-            checkCycle(i, j-1, n, m, tar, vis, grid) ;
-        
-        grid[i][j] = ch ;    
-        return ans ;
-    }
-         
 public:
     bool containsCycle(vector<vector<char>>& grid) {
-        //DFS
-        int n = grid.size() ;
-        int m = grid[0].size() ;
+        int n = grid.size(), m = grid[0].size() ;
+        DisjoinSet ds(n*m +1) ;
         vector<vector<int>> vis(n, vector<int>(m, 0)) ;
         
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(!vis[i][j]){
-                    if(checkCycle(i, j, n, m, grid[i][j], vis, grid))
+                int node = i*m + j;
+                vis[i][j] = 1 ;
+                
+                for(int ind=0; ind<4; ind++){
+                    int new_row = i + dr[ind] ;
+                    int new_col = j + dc[ind] ;
+                    
+                    int adjNode = new_row*m + new_col ;
+                    if(!inRange(new_row, new_col, n, m) || grid[i][j] != grid[new_row][new_col] || vis[new_row][new_col] ==1)
+                        continue ;
+                    
+                    if(ds.findParent(node) == ds.findParent(adjNode)){
+                        // cout<<node<<"  "<<adjNode<<"      "<<i<<" "<<j<<" -> "<<"true"<<endl ;
                         return true ;
+                    }
+                    
+                    ds.unionBySize(node, adjNode) ;
                 }
             }
         }
