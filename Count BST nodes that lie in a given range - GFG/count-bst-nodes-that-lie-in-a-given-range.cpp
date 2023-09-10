5fd88f06c241fba477783b85242d087091eea3fa
komalkumari12 <91238713+komalkumari12@users.noreply.github.com>
2023-09-10 20:58:01 +0530

    Added solution - LeetHub

--- Count BST nodes that lie in a given range - GFG/count-bst-nodes-that-lie-in-a-given-range.cpp
+++ Count BST nodes that lie in a given range - GFG/count-bst-nodes-that-lie-in-a-given-range.cpp
@@ -1,4 +1,4 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -83,38 +83,40 @@ Node* buildTree(string str)
 }
 
 
- // } Driver Code Ends
-
+// } Driver Code Ends
 //Function to count number of nodes in BST that lie in the given range.
 class Solution{
 public:
     int getCount(Node *root, int l, int h)
     {
-        queue<Node*> q;
-        q.push(root) ;
-        
-        int count =0 ;
-        while(!q.empty()){
-            int size = q.size() ;
-            
-            for(int i=0; i<size; i++){
-                Node* node = q.front() ;
-                q.pop() ;
-                
-                if(node->data >= l && node->data <= h)
-                    count++ ;
-                
-                if(node->left) q.push(node->left) ;
-                if(node->right) q.push(node->right) ;
-            }
-        }
-        
-        return count ;
+      // BFS
+      if(!root) return 0 ;
+      
+      queue<Node*> q ;
+      q.push(root) ;
+      
+      int count =0 ;
+      while(!q.empty()){
+          int size = q.size() ;
+          
+          for(int i=0; i<size; i++){
+              Node* node = q.front() ;
+              q.pop() ;
+              
+              if(node->data >= l && node->data <= h)    count++ ;
+              
+              if(node->left)     q.push(node->left) ;
+              if(node->right)    q.push(node->right) ;
+              
+          }
+      }
+      
+      return count ;
     }
 };
 
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 
 int main()
 {
@@ -134,4 +136,5 @@ int main()
 	}
 	return 1;
 }
-  // } Driver Code Ends
+
+// } Driver Code Ends
