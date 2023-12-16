e6e166fd968b7e590ff5fcc89701346ee8659cd1
komalkumari12 <91238713+komalkumari12@users.noreply.github.com>
2023-12-16 11:19:51 +0530

    Added solution - LeetHub

--- Number of Provinces - GFG/number-of-provinces.cpp
+++ Number of Provinces - GFG/number-of-provinces.cpp
@@ -6,78 +6,71 @@ using namespace std;
 // } Driver Code Ends
 //User function Template for C++
 
-class DisjoinSet{
-    public:
-        vector<int> parent, rank, size ;
-    
-        DisjoinSet(int n){
-            parent.resize(n+1) ;
-            rank.resize(n+1, 0) ;
-            size.resize(n+1, 1) ;
-            
-            for(int i=0; i<n+1; i++)
-                parent[i] = i ;
-        }
-        
-        int findParent(int node){
-            if(parent[node] == node)
-                return node ;
-            return parent[node] = findParent(parent[node]) ;
-        }
-        
-        void unionByRank(int u, int v){
-            int par_u = findParent(u) ;
-            int par_v = findParent(v) ;
-            
-            if(par_u == par_v)  return ;
-            if(rank[par_v] < rank[par_u]){
-                parent[par_v] = par_u ;
-            }
-            else if(rank[par_v] > rank[par_u]){
-                parent[par_u] = par_v ;
-            }
-            else{
-                parent[par_v] = par_u ;
-                rank[par_u]++ ;
-            }
-        }
-        
-        void unionBySize(int u, int v){
-            int par_u = findParent(u) ;
-            int par_v = findParent(v) ;
-            
-            if(par_u == par_v)  return ;
-            if(size[par_u] < size[par_v]){
-                parent[par_u] = par_v ;
-                size[par_v] += size[par_u] ;
-            }
-            else{
-                parent[par_v] = par_u ;
-                size[par_u] += size[par_v] ;
-            }
-        }
-};
-
 class Solution {
   public:
-    int numProvinces(vector<vector<int>> adj, int V) {
-        DisjoinSet ds(V) ;
+    // void bfs(int node, vector<int>& vis, vector<int> adj[]){
+    //     queue<int> q ;
+    //     q.push(0) ;
+    //     vis[node] = 1 ;
         
-        for(int i=0; i<V; i++){
-            for(int j=0; j<V; j++){
-                if(adj[i][j] == 1){
-                    ds.unionBySize(i, j) ;
+    //     while(!q.empty()){
+    //         int node = q.front() ;
+    //         q.pop() ;
+            
+    //         vis[node] = 1 ;
+            
+    //         for(auto it: adj[node]){
+    //             if(!vis[it]){
+    //                 vis[it] = 1 ;
+    //                 q.push(it) ;
+    //             }
+    //         }
+    //     }
+    // }
+    
+    void dfs(int node, vector<int>& vis, vector<int> adj[]){
+        vis[node] = 1 ;
+        
+        for(auto it: adj[node]){
+            if(!vis[it]){
+                dfs(it, vis, adj) ;
+                vis[it] = 1 ;
+            }
+        }
+    }
+    
+    int numProvinces(vector<vector<int>> graph, int V) {
+        // DFS
+        vector<int> adj[V] ;
+        
+        for(int i=0; i<graph.size(); i++){
+            for(int j=0; j<graph[i].size(); j++){
+                if(graph[i][j] == 1 && i != j){
+                    adj[i].push_back(j) ;
+                    adj[j].push_back(i) ;
                 }
             }
         }
         
-        int count =0 ;
+        // for(int i=0; i<adj.size(); i++){
+        //     cout<<i<<" -> " ;
+        //     for(auto it                                                                                                                                                                                          ){
+        //         cout<<j<<" ";
+        //     }
+        //     cout<<endl ;
+        // }
+        
+        vector<int> vis(V, 0) ;
+        int components = 0 ;
+        
         for(int i=0; i<V; i++){
-            if(ds.parent[i] == i)
-                count++ ;
+            if(vis[i] == 0){
+                components++ ;
+                dfs(i, vis, adj) ;
+            }
         }
         
-        return count ;
+        return components ;
     }
 };
 
